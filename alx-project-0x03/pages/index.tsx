72f5d264bd10b9d0e6a0c72af48b1eb8830eba99
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 17:26:34 +0100

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,10 +1,9 @@
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Welcome to Splash App</h1>
+    <div>
+      <h1> Welcome to Splash App </h1>
     </div>
   );
 };
 
 export default Home;
-
