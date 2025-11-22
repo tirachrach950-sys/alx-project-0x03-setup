08afc2f9b74cbf6d70a3c8de9d4975715f04e124
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-22 17:01:20 +0100

    Update Header.tsx

--- alx-project-0x03/components/layouts/Header.tsx
+++ alx-project-0x03/components/layouts/Header.tsx
@@ -1,23 +1,24 @@
 import Link from "next/link";
 import Button from "../common/Button";
+import { FaUser } from "react-icons/fa"; // example icon for button
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed w-full bg-white shadow-md">
+    <header className="fixed w-full bg-white shadow-md z-50">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
         <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
           Splash App
         </Link>
 
-        {/* Button Group */}
-        <div className="flex gap-4">
-          <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-          <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
-        </div>
-      </div>
-    </header>
-  );
-};
+        <div className="flex gap-4 items-center">
+          {/* Sign In button */}
+          <Button
+            buttonLabel="Sign In"
+            buttonBackgroundColor="red"
+            action={() => console.log("Sign In clicked")}
+          />
 
-export default Header;
+          {/* Sign Up button with icon */}
+          <Button
+            buttonLabel="Sign Up"
 
