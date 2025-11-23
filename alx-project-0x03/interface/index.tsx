985599a877a783f9a9db53835ac45213b8052029
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-23 15:21:21 +0100

    Update index.tsx

--- alx-project-0x03/interface/index.tsx
+++ alx-project-0x03/interface/index.tsx
@@ -1,1 +1,22 @@
+// interface/index.tsx
+
+import { ReactNode } from "react";
+
+// Button.tsx
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
+
+// Layout.tsx
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+// pages/index.tsx
+export interface PageRouteProps {
+  pageRoute: string;
+}
 
