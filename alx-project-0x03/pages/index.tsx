5bb45cf8b9316f323d88d49c878e8e74781804d3
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-23 15:09:48 +0100

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,9 +1,17 @@
-const Home = () => {
-  return (
-    <div>
-      <h1> Welcome to Splash App </h1>
-    </div>
-  );
-};
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
 
-export default Home;
+interface PageRouteProps {
+  pageRoute: string;
+}
+
+export default function Home() {
+  const router = useRouter();
+
+  // Imperative routing with useRouter
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false });
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
